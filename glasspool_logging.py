3e1720536cc4cf5ed9ad2071bed9331779605a47
yushizhao <yushizhao@outlook.com>
2019-11-11 17:32:03 +0800

    logging

---
+++ glasspool_logging.py
@@ -0,0 +1,29 @@
+import logging
+import functools
+
+def start_log(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+ 
+    rht = logging.handlers.TimedRotatingFileHandler(f"{name}.log", 'D')
+    fmt = logging.Formatter("%(asctime)s %(filename)s %(lineno)s \
+      %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+    rht.setFormatter(fmt)
+    logger.addHandler(rht)
+
+    return logger
+
+glassflow_log = start_log("glassflow")
+
+import functools
+
+def glassflow(func):
+    @functools.wraps(func)
+    def glassflow_decorator(*args, **kwargs):
+        # Do something before
+        glassflow_log.info(args, kwargs)
+        value = func(*args, **kwargs)
+        # Do something after
+        glassflow_log.info(value)
+        return value
+    return glassflow_decorator
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from glasspool_logging import glassflow, glassflow_log
 
+app = FastAPI()
 
 @app.get("/")
 async def root():
+    glassflow_log.debug("test")
     return {"message": "Hello World"}
