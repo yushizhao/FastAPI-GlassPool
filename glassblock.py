48739cc4cfaca8ff78146b672bda3e96cb3d4fa8
yushizhao <yushizhao@outlook.com>
2019-11-18 17:14:35 +0800

    add memo

--- glassblock.py
+++ glassblock.py
@@ -1,9 +1,12 @@
 import time
-from secrets import token_bytes
+from secrets import token_bytes, token_urlsafe
 
 from eth_utils import keccak
 from eth_utils.conversions import to_hex
 from eth_keys import keys
+
+def get_eos_address():
+    return f"glasspool-deposit[{token_urlsafe(6)}]"
 
 def get_eth_address():
     pk = keys.PrivateKey(token_bytes(32))
@@ -12,15 +15,17 @@ def get_eth_address():
 class GlassBlock:
     def __init__(self, type: str):
         # to do: extend to other types
-        if type == "ETH":
-            self.chain = "ETH"
-            self.blocktime = 12.5
-            self.intercept = 116966214
-            self.get_address = get_eth_address
+        if type == "EOS":
+            self.chain = "EOS"
+            self.blocktime = 2
+            self.intercept = 696546381
+            self.use_memo = True
+            self.get_address = get_eos_address
         else:
             self.chain = "ETH"
             self.blocktime = 12.5
             self.intercept = 116966214
+            self.use_memo = False
             self.get_address = get_eth_address
     
     def get_number_and_hash(self):
